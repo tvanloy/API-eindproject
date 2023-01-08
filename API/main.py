3203e1a588d268a30e04d58bb33eebb527659c02
Tristan <91123059+tvanloy@users.noreply.github.com>
2023-01-08 20:20:27 +0100

    Update main.py

--- API/main.py
+++ API/main.py
@@ -2,14 +2,36 @@ import fastapi
 import sqlalchemy.orm as orm
 from fastapi import FastAPI
 from typing import List
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordRequestForm
+
 import os
 import crud
 import schemas
+import auth
 
 if not os.path.exists('./sqlitedb'):
     os.makedirs('./sqlitedb')
 
 app = FastAPI()
+
+origins = [
+    "http://localhost/",
+    "http://localhost:8080/",
+    "http://localhost:63342",
+    "https://localhost.tiangolo.com/",
+    "http://127.0.0.1:5500/",
+    "http://127.0.0.1:8080",
+    "",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 crud.create_database()
 
@@ -28,6 +50,7 @@ def create_user(user: schemas.UserCreate, db: orm.Session = fastapi.Depends(crud
 def read_users(skip: int = 0,
                limit: int = 10,
                db: orm.Session = fastapi.Depends(crud.get_db),
+               token: str = fastapi.Depends(auth.oauth2_scheme)
                ):
     users = crud.get_users(db=db, skip=skip, limit=limit)
     return users
@@ -70,3 +93,22 @@ def delete_quote(quote_id: int, db: orm.Session = fastapi.Depends(crud.get_db)):
 @app.put("/quotes/{post_id}", response_model=schemas.Quote)
 def update_quote(quote_id: int, quote: schemas.QuoteCreate, db: orm.Session = fastapi.Depends(crud.get_db)):
     return crud.update_quote(db=db, quote=quote, quote_id=quote_id)
+
+
+@app.post("/token")
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = fastapi.Depends(),
+                           db: orm.Session = fastapi.Depends(crud.get_db)):
+    # Try to authenticate the user
+    user = auth.authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise fastapi.HTTPException(
+            status_code=401,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    # Add the JWT case sub with the subject(user)
+    access_token = auth.create_access_token(
+        data={"sub": user.email}
+    )
+    # Return the JWT as a bearer token to be placed in the headers
+    return {"access_token": access_token, "token_type": "bearer"}
